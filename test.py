b88e446ed2d057b76703c2f86557a2527daff7a2
Bohdan213 <bogdanpeleh213@gmail.com>
2022-02-20 23:42:49 +0200

    4

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ def main():
         prim_time = prim(i, graph)
         kraskal_time = kraskal(i, graph)
         data.append((i, prim_time, kraskal_time))
-    with open('rezults', mode='w') as file:
+    with open('rezults.csv', mode='w') as file:
         file_writer = csv.writer(file, delimiter=',', quoting=csv.QUOTE_MINIMAL)
         for rez in data:
             file_writer.writerow([rez[0], rez[1], rez[2]])
