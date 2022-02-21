7bf8dc85800f09730d00417d126e329a89665d88
Maksym Mykhasiuta <maksym.mykhasiuta@ucu.edu.ua>
2022-02-21 16:37:50 +0200

    Plotting added

---
+++ plotting.py
@@ -0,0 +1,58 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import matplotlib.style as style
+
+# style.use('dark_background')
+def draw_plots():
+    style.use('seaborn-dark-palette')
+
+    data = pd.read_csv('results.csv')
+    
+    plt.figure(1)
+
+    plt.plot(data.edges, data.prim_1, 'b.-', label='Prim 0.1', color='green')
+    plt.plot(data.edges, data.kruskal_1, 'b.-', label='Kruskal 0.1', color='red')
+    plt.suptitle('(density: 0.1)')
+    plt.title('Algorithm Comparison ', fontdict={
+            'fontsize': 15, 'fontweight': 'bold'})
+    plt.xlabel('Number of vertices')
+    plt.ylabel('Time')
+    plt.xticks([0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000])
+    max_1 = max(max(data.kruskal_1), max(data.prim_1))
+    plt.yticks([0, max_1/2, max_1])
+    plt.legend()
+    plt.figure(2)
+
+    plt.plot(data.edges, data.prim_5, 'b.-', label='Prim 0.5', color='green')
+    plt.plot(data.edges, data.kruskal_5, 'b.-',
+            label='Kruskal 0.5', color='red')
+    plt.suptitle('(density: 0.5)')
+    plt.title('Algorithm Comparison ', fontdict={
+            'fontsize': 15, 'fontweight': 'bold'})
+    plt.xlabel('Number of vertices')
+    plt.ylabel('Time')
+    plt.xticks([0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000])
+    max_5 = max(max(data.kruskal_5), max(data.prim_5))
+    plt.yticks([0, max_5/2, max_5])
+    plt.legend()
+
+    plt.figure(3)
+    plt.plot(data.edges, data.prim_9, 'b.-', label='Prim 0.9', color='green')
+    plt.plot(data.edges, data.kruskal_9, 'b.-', label='Kruskal 0.9', color='red')
+    plt.suptitle('(density: 0.9)')
+    plt.title('Algorithm Comparison ', fontdict={
+            'fontsize': 15, 'fontweight': 'bold'})
+    plt.xlabel('Number of vertices')
+    plt.ylabel('Time')
+    plt.xticks([0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000])
+    max_9 = max(max(data.kruskal_9), max(data.prim_9))
+    plt.yticks([0, max_9/2, max_9])
+    plt.legend()
+
+    plt.figure(1).savefig('images/density1.jpg')
+    plt.figure(2).savefig('images/density5.jpg')
+    plt.figure(3).savefig('images/density9.jpg')
+    # plt.show()
+
+if __name__ == '__main__':
+    draw_plots()
